41ed24ac8ed01939613951e2678c214d25768334
Javier Benito Santoni <javierbenitosantoni@icloud.com>
2019-11-26 11:53:33 +0100

    App terminada

--- app/src/main/java/com/example/quidditchscoreapp/MainActivity.java
+++ app/src/main/java/com/example/quidditchscoreapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -16,27 +17,32 @@ public class MainActivity extends AppCompatActivity {
     private boolean cincoA = false;
     private boolean diezB = false;
     private boolean cincoB = false;
+    private boolean snitchB = false;
+    private boolean snitchA = false;
+
     TextView puntuacionA;
     Button BdiezA;
     Button BcincoA;
-    Button snitchA;
+    Button BsnitchA;
     TextView puntuacionB;
     Button BdiezB;
     Button BcincoB;
-    Button snitchB;
+    Button BsnitchB;
+    Button Breset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         puntuacionA = findViewById(R.id.PuntosA);
-        snitchA = (Button) findViewById(R.id.snitchA);
+        BsnitchA = (Button) findViewById(R.id.snitchA);
         puntuacionB = findViewById(R.id.PuntosB);
-        snitchB = (Button) findViewById(R.id.snitchB);
+        BsnitchB = (Button) findViewById(R.id.snitchB);
         BdiezA = (Button) findViewById(R.id.DiezMasA);;
         BcincoA = (Button) findViewById(R.id.CincoMasA);;
         BdiezB = (Button) findViewById(R.id.DiezMasB);;
         BcincoB = (Button) findViewById(R.id.CincoMasB);;
+        Breset = (Button) findViewById(R.id.Resetear);;
 
         View.OnClickListener suma = new View.OnClickListener() {
             @Override
@@ -62,41 +68,64 @@ public class MainActivity extends AppCompatActivity {
         BdiezB.setOnClickListener(suma);
         BcincoB.setOnClickListener(suma);
 
-        snitchA.setOnClickListener(new View.OnClickListener() {
+
+        View.OnClickListener snitch = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(v.getId() == R.id.snitchA) {
+                    snitchA = true;
+                    snitch();
+                } else if (v.getId() == R.id.snitchB) {}
+                snitchB = true;
+                snitch();
+        }};
+        BsnitchA.setOnClickListener(snitch);
+        BsnitchB.setOnClickListener(snitch);
 
-            }
-        });
-        snitchB.setOnClickListener(new View.OnClickListener() {
+
+        View.OnClickListener reset = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-            }
-        });
+            resetear();
+            }};
+        Breset.setOnClickListener(reset);
     }
-
     public void incrementar() {
         if (diezA == true) {
             puntosA = puntosA + 10;
             diezA = false;
+            puntuacionA.setText(puntosA + "");
         } else if(cincoA == true) {
             puntosA = puntosA + 5;
             cincoA = false;
+            puntuacionA.setText(puntosA + "");
         } else if (diezB == true) {
             puntosB = puntosB + 10;
             diezB = false;
+            puntuacionB.setText(puntosB + "");
         } else if (cincoB == true){
             puntosB = puntosB + 5;
             cincoB = false;
+            puntuacionB.setText(puntosB + "");
         }
     }
 
     public void resetear() {
-
+        puntosA = 0;
+        puntosB = 0;
+        puntuacionA.setText(puntosA + "");
+        puntuacionB.setText(puntosB + "");
     }
 
     public void snitch() {
-
+    if(snitchA == true) {
+        Toast ganaA = Toast.makeText(getApplicationContext(),"Ha ganado A", Toast.LENGTH_SHORT);
+        ganaA.show();
+        snitchA = false;
+    }else {
+        Toast ganaB = Toast.makeText(getApplicationContext(),"Ha ganado B", Toast.LENGTH_SHORT);
+        ganaB.show();
+        snitchB = false;
+    }
     }
 }
